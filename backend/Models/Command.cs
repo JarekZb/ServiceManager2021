4e6eeebe16806b8b491be71f18ff8c4f4c5f03ab
JarekZb <jarek.zbaski@wp.pl>
2021-04-19 10:02:06 +0200

    Revert "init_backend"
    This reverts commit 398dfb54eb9fd1420dd29323d495c087b8a1b1ab.

--- backend/Models/Command.cs
+++
@@ -1,9 +0,0 @@
-namespace CommandApi.Models
-{
-    public class Command{
-        public int Id{get;set;}
-        public string HowTo{get;set;}
-        public string Line{get;set;}
-        public string Platform{get;set;}
-    }
-}
